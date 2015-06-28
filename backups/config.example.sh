db512c7ffaee724b488d5eb4a7a29c827a769dd9
Rachel Fish <itsrachelfish@gmail.com>
2015-06-28 08:38:13 -0600

    Added config example and cronjobs

---
+++ backups/config.example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# copy this file to "config.sh" and change the following values
+
+username="remote ssh username"
+host="remote ssh host"
+remote_path="remote/path/where/files/are/being/saved/"
+local_path="local/path/where/files/are/being/copied/"
