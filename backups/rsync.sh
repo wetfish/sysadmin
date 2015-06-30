ed5a971781957926f5980b80cc817ea00d6d59f3
Rachel Fish <itsrachelfish@gmail.com>
2015-06-29 23:13:32 -0600

    Fixed relative config path, fixed crontab example

--- backups/rsync.sh
+++ backups/rsync.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Load connection details from config.sh
-source config.sh
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source $dir/config.sh
 rsync -azP $username@$host:$remote_path $local_path
