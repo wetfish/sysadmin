5d6d0c5c36e38dca5de2989ed6bf6992c73ee4c2
Rachel Fish <itsrachelfish@gmail.com>
2015-07-03 16:18:18 -0600

    super derp

--- mumble/stopproxy.sh
+++ mumble/stopproxy.sh
@@ -3,11 +3,8 @@
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $dir/config.sh
 
-iptables -t nat -D PREROUTING --dst $YourIP -p tcp --dport $YourPort -j DNAT --to-destination $TargetIP:$TargetPort
-iptables -t nat -D POSTROUTING -p tcp --dst $TargetIP --dport $TargetPort -j SNAT --to-source $YourIP
-iptables -t nat -D OUTPUT --dst $YourIP -p tcp --dport $YourPort -j DNAT --to-destination $TargetIP:$TargetPort
+iptables -t nat -D PREROUTING --dst $CurrentIP -p tcp --dport $CurrentPort -j DNAT --to-destination $TargetIP:$TargetPort
+iptables -t nat -D POSTROUTING -p tcp --dst $TargetIP --dport $TargetPort -j SNAT --to-source $CurrentIP
 
-iptables -t nat -D PREROUTING --dst $YourIP -p udp --dport $YourPort -j DNAT --to-destination $TargetIP:$TargetPort
-iptables -t nat -D POSTROUTING -p udp --dst $TargetIP --dport $TargetPort -j SNAT --to-source $YourIP
-iptables -t nat -D OUTPUT --dst $YourIP -p udp --dport $YourPort -j DNAT --to-destination $TargetIP:$TargetPort
-
+iptables -t nat -D PREROUTING --dst $CurrentIP -p udp --dport $CurrentPort -j DNAT --to-destination $TargetIP:$TargetPort
+iptables -t nat -D POSTROUTING -p udp --dst $TargetIP --dport $TargetPort -j SNAT --to-source $CurrentIP
