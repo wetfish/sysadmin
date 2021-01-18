dd6c303be2d3a50e9c05fca6ebf27adb3a481c3e
halfwit <michaelmisch1985@gmail.com>
2021-01-18 07:27:38 +0000

    Add in online, move to config/nginx directory

--- deployment/config/fetch_sources.sh
+++ deployment/config/fetch_sources.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 test -d /git/sources/classic || (cd /git/sources && git clone https://github.com/wetfish/classic)
-test -d /git/sources/ads || (cd /git/sources && git clone https://github.com/wetfish/ads)
 test -d /git/sources/memes || (cd /git/sources && git clone https://github.com/itsrachelfish/memes)
+test -d /git/sources/online || (cd /git/sources && git clone https://github.com/wetfish/online)
