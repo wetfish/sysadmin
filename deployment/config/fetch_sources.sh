25c0c508e5d90588f5e042102fe93432b8c2a928
halfwit <michaelmisch1985@gmail.com>
2021-01-27 20:36:42 +0000

    Add in wet.fish

--- deployment/config/fetch_sources.sh
+++ deployment/config/fetch_sources.sh
@@ -2,3 +2,4 @@
 
 test -d /git/sources/classic || (cd /git/sources && git clone https://github.com/wetfish/classic)
 test -d /git/sources/online || (cd /git/sources && git clone https://github.com/wetfish/online)
+test -d /git/sources/corporate || (cd /git/sources && git clone https://github.com/wetfish/wet.fish corporate
